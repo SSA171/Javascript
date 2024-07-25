0127a7c01cdb17dae3e996f49614c6f01f9c84ce
Sas <90267986+SSA171@users.noreply.github.com>
2024-07-25 20:43:58 +0200

    dAy 2

---
+++ jasc.js
@@ -0,0 +1,15 @@
+let age = 21 // let can only we used once to declare variable
+
+let next = age + 1 // has let because we are defining a new variable next
+
+
+console.log(age) //console.log() similar to print
+console.log(next) //console.log() similar to print
+
+3 + 4 // Adds 3 and 4: 7
+
+let x = 2
+let y = 3
+x+y //Adds x and y, so in this example 2 + 3: 5
+
+x*y // Multiplies x and y, so in this example: 2 * 3: 6
